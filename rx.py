bcba20c1385736806b3209663c3e4357d8f47f50
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-19 17:11:56 +0200

    Update rx.py

--- rx.py
+++ rx.py
@@ -18,7 +18,7 @@ import serial_asyncio
 class SBUSReceiver:
     class SBUSFramer(asyncio.Protocol):
 
-        START_BYTE = 0xf0
+        START_BYTE = 0x0f
         END_BYTE = 0x00
         SBUS_FRAME_LEN = 25
 	#\xf8.\x00
