035977c1043a114f980f5c1b78f7b15f3ece2f8a
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-19 21:20:06 +0200

    Update rx.py

--- rx.py
+++ rx.py
@@ -112,8 +112,12 @@ class SBUSReceiver:
 
         def __repr__(self):
             #return ",".join(str(ch) for ch in self.sbusChannels)
-            return (self.sbusChannels)
-
+            str=""
+            for ch,i in self.sbusChannels:
+		if i != 1 or i != 2:
+			str += str(ch)
+            return str
+            
     def __init__(self):
         self._transport = None
         self._protocol = None
