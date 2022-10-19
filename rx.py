5dd66225d9a3409d1323ff66e2bfe7192dc162f0
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-19 18:17:43 +0200

    Update rx.py

--- rx.py
+++ rx.py
@@ -69,9 +69,10 @@ class SBUSReceiver:
             #self.sbusChannels[2] = ((channel_sum[3]>>6 | channel_sum[4]<<2 | channel_sum[5]<<10) & 0x07FF);
             #self.sbusChannels[3] = ((channel_sum[5]>>1 | channel_sum[6]<<7) & 0x07FF);
             #print (frame[0:42])	
-            channel_sum = int.from_bytes(frame[1:23], byteorder="little")		
+            channel_sum = frame[1:23]		
             for ch in range(0, 16):
                 self.sbusChannels[ch] = channel_sum & 0x7ff
+		self.sbusChannels[ch] = int.from_bytes(self.sbusChannels[ch], byteorder="little")
                 channel_sum = channel_sum >> 11
 
 
