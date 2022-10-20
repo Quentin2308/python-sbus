7d110285cc37d5f9e5a1682a5961c0e82b549c09
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-20 16:08:16 +0200

    Update sbus.py

--- sbus.py
+++ sbus.py
@@ -11,7 +11,7 @@ class SBUSReceiver:
     
     class SBUSFramer(asyncio.Protocol):
 		
-        START_BYTE = 0xfe 
+        START_BYTE = 0xf8
         #END_BYTE = 0x00
         SBUS_FRAME_LEN = 25
 
