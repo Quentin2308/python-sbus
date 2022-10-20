6c501d9014e832288b47c0645484294b691bcfb3
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-20 14:53:44 +0200

    Update sbus.py

--- sbus.py
+++ sbus.py
@@ -69,10 +69,10 @@ class SBUSReceiver:
             asyncio.get_running_loop(),
             SBUSReceiver.SBUSFramer,
             port,
-            baudrate=100000)#,
-            #parity=serial.PARITY_EVEN,
-            #stopbits=serial.STOPBITS_TWO,
-            #bytesize=serial.EIGHTBITS)
+            baudrate=100000,
+            parity=serial.PARITY_EVEN,
+            stopbits=serial.STOPBITS_TWO,
+            bytesize=serial.EIGHTBITS)
         print (receiver._protocol)
 	
         return receiver
