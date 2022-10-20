e34677aaf9b9278d5efd9d2c1813d44e9354ae48
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-20 02:10:41 +0200

    Update rx.py

--- rx.py
+++ rx.py
@@ -27,7 +27,7 @@ _UART_FRAME_LENGTH = 12
 class SBUSReceiver:
     class SBUSFramer(asyncio.Protocol):
 
-        START_BYTE = 0xf6
+        START_BYTE = 0xee
         END_BYTE = 0x00
         SBUS_FRAME_LEN = 35
         #SBUS_FRAME_LEN = 22
